78155800e7ab3c1c6aeb8fb73b55438f2bed509d
Christine <38509948+chrissymitsch@users.noreply.github.com>
2019-12-04 13:22:17 +0100

    Day 4

---
+++ Day4/1.js
@@ -0,0 +1,25 @@
+counter = 0;
+for (var i = 236491; i < 713787; i++) {
+	var stringify = i + "";
+	var bedingung1 = []; // 22
+  var bedingung2 = []; // 123
+  for (var j = 0; j < stringify.length; j++) {
+  	if (stringify[j] == stringify[j + 1]) {
+    	bedingung1[j] = "true";
+    } else {
+    	bedingung1[j] = "false";
+    }
+    if (j < stringify.length - 1) {
+      if (parseInt(stringify[j], 10) <= parseInt(stringify[j + 1], 10)) {
+        bedingung2[j] = "true";
+      } else {
+        bedingung2[j] = "false";
+      }
+    }
+  }
+  if (bedingung1.includes("true") && !bedingung2.includes("false")) {
+  	counter++;
+  }
+}
+
+console.log(counter);
